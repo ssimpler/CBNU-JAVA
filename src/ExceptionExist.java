aa0f28a0440c86d2e7cf0ef8dfb60f2a6fd6d280
Lee Jae SUng <jasonlee@cbnu.ac.kr>
2017-10-27 16:24:50 +0900

    kimneunghwan

---
+++ src/ExceptionExist.java
@@ -0,0 +1,14 @@
+class ExceptionExist 
+{
+	public static void main(String[] args) 
+	{
+		func();                 //메소드 호출
+	}
+	
+	static void func() 
+	{
+		int i = 1;
+		int j = 0;
+		System.out.println(i/j);    // 1을 0으로 나눈다. 예외 발생
+	}
+}
